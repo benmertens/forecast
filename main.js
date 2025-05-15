c6436107d70dc0df5a402b1ff10f20babd0d11b4
benmertens <ben.mertens@student.uibk.ac.at>
2025-05-15 16:46:09 +0200

    Klick auf Innsbruck simulieren

--- main.js
+++ main.js
@@ -35,3 +35,11 @@ L.control.scale({
 map.on("click", function(evt){
     console.log(evt, evt.latlng);
 })
+
+// Klick auf Innsbruck simulieren
+map.fire("click", {
+    latlng:{
+        lat: ibk.lat,
+        lng: ibk.lng,
+    }
+})
